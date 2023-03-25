8745d1cab0c01723a738e62abe79e607b19fd590
Felipe <107360437+felipesphair@users.noreply.github.com>
2023-03-25 17:05:18 -0300

    Update App.py

--- App.py
+++ App.py
@@ -110,3 +110,19 @@ while True:
         excluir()
     else:
         print("opçao invalida")    
+        
+#import sqlite3
+
+#banco = sqlite3.connect('primeiro_banco.db')
+
+#cursor = banco.cursor()
+
+#cursor.execute("CREATE TABLE pessoas(nome text,idade integer, email text)")
+
+#cursor.execute("INSERT INTO pessoas VALUES('Felipe', 40,'sphairfelipe@gmail.com')")
+
+#banco.commit()
+
+#cursor.execute("SELECT * FROM pessoas")
+
+#print(cursor.fetchall())
